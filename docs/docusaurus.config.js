82c226e428876afd30ff9c2850489683c5e2f96d
brndhpkn <98061326+brndhpkn@users.noreply.github.com>
2022-03-31 11:15:23 -0500

    reset

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,65 +1,53 @@
 module.exports = {
-  title: "Service Workbench on AWS",
-  tagline: "A web portal for researchers to accelerate their time to science",
+  title: "Service Workbench Platform",
+  tagline: "Researching data made easy",
   url: "https://your-docusaurus-test-site.com",
   baseUrl: "/",
   favicon: "img/favicon.ico",
   organizationName: "aws", // Usually your GitHub org/user name.
-  projectName: "service-workbench-on-aws", // Usually your repo name.
+  projectName: "docusaurus", // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: "Service Workbench on AWS",      
-      logo: {
-        alt: 'Site Logo',
-        src: "img/AWS_logo_RGB.svg",
-        target: '_self',
-        width: 32,
-        height: 32,
-      },
-    footer: {
-        style: "dark",
-        links: [
-          {
-            title: "AWS",
-            items: [
-              {
-                label: "AWS Docs",
-                href: "https://docs.aws.amazon.com/solutions/latest/service-workbench-on-aws/overview.html",
-              },
-              {
-                label: "Sales Channel",
-                href: "https://aws.amazon.com/government-education/research-and-technical-computing/service-workbench/",
-              },
-            ],
-          },
-          {
-            title: "Social",
-            items: [
-              {
-                label: "GitHub",
-                href: "https://github.com/awslabs/service-workbench-on-aws",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/awscloud",
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Amazon Web Services, Inc. or its affiliates. All rights reserved.`,
-      },
-    get footer() {
-      return this._footer;
+      title: "Home",
     },
-    set footer(value) {
-      this._footer = value;
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
+            },
+          ],
+        },
+        {
+          title: "Social",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
     },
   },
   presets: [
     [
       "@docusaurus/preset-classic",
       {
-        docs: {          
+        docs: {
           routeBasePath: "/",
           sidebarPath: require.resolve("./sidebars.js"),
           editUrl: "https://github.com/awslabs/go-research-on-aws/website/",
@@ -71,5 +59,4 @@ module.exports = {
       },
     ],
   ],
-},
-}
+};

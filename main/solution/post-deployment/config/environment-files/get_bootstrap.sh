cb942fa557ce64abf566428449f46cfb9b053696
dengmingtong <dengmingtong@hotmail.com>
2022-07-03 09:17:05 +0800

    make study data workable in beijing/ningxia/hongkong regions

--- main/solution/post-deployment/config/environment-files/get_bootstrap.sh
+++ main/solution/post-deployment/config/environment-files/get_bootstrap.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 bootstrap_s3_location="$1"
 s3_mounts="$2"
+region="$3"
 
 INSTALL_DIR="/usr/local/share/workspace-environment"
 
 # Download instance files and execute bootstrap script
 sudo mkdir "$INSTALL_DIR"
-sudo aws s3 sync "$bootstrap_s3_location" "$INSTALL_DIR"
+sudo aws s3 sync --region "$region" "$bootstrap_s3_location" "$INSTALL_DIR"
 
 bootstrap_script="$INSTALL_DIR/bootstrap.sh"
 if [ -s "$bootstrap_script" ]

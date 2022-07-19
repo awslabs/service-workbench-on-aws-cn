f85c598567654ec67d02ddc563ee5011c170d031
6zkf6zs05ryz <6zkf6zs05ryz@gmail.com>
2022-07-19 16:57:42 +0800

    Disable i18next debug

--- main/solution/ui/src/i18next.js
+++ main/solution/ui/src/i18next.js
@@ -14,7 +14,7 @@ i18next
     ns: ['common'],
     defaultNS: 'common',
     fallbackLng: 'en-US',
-    debug: true,
+    debug: false,
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default

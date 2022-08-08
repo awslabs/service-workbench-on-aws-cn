fc5555f591356b5222e302d118b6bf784a40d087
dengmingtong <dengmingtong@hotmail.com>
2022-08-08 22:50:53 +0800

    fix the issue #54

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -52,7 +52,7 @@ module.exports = {
         docs: {
           routeBasePath: "/",
           sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: "https://github.com/awslabs/go-research-on-aws/website/",
+          editUrl: "https://github.com/awslabs/service-workbench-on-aws-cn/edit/develop/docs/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
